d2eaa6f00f1ca2c741371ffd35cda56ec37a83c5
EmaGaleas <131042659+EmaGaleas@users.noreply.github.com>
2024-03-01 08:55:49 -0600

    cositas (#1)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QImage>
+#include <QPixmap>
+#include <QLabel>
+#include <istream>
+#include <iostream>
+#include <string>
+#include <sstream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QImage menu(":/prefix/principal proyecto.png");
+    ui->lbl_pngMenu->setPixmap(QPixmap::fromImage(menu));
+    ui->lbl_b1M->setStyleSheet("background-color: #062458;");
+    ui->lbl_b2M->setStyleSheet("background-color: #062458;");
+
+
 }
 
 MainWindow::~MainWindow()
