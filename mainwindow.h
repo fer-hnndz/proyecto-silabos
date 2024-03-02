f76d05a5b2b43fc018a0d33edb19660b39439fae
EmaGaleas <131042659+EmaGaleas@users.noreply.github.com>
2024-03-02 12:09:30 -0600

    cframe  Entrega (#2)
    pendiente enable al cierre

--- mainwindow.h
+++ mainwindow.h
@@ -16,7 +16,23 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_btn_entregarM_clicked();
+    void on_btn_revision_clicked();
+    void on_btn_dashboardM_clicked();
+    void on_btn_sesion_clicked();
+    void on_tabWidget_tabBarClicked(int index);
+    void on_tabWidget_currentChanged(int index);
+    void on_cb_facultadE_currentIndexChanged(const QString &arg1);
+
+    void on_btn_silaboE_clicked();
+
+    void on_btn_archivoE_clicked();
+
 private:
     Ui::MainWindow *ui;
+    bool loginDocente=false;
+    bool loginRevision=false;
+    void limpiarEntrega();
 };
 #endif // MAINWINDOW_H
