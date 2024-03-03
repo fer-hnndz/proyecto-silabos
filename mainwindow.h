84e9a33320edc582c7fe3415ac22f62029e24b8d
EmaGaleas <131042659+EmaGaleas@users.noreply.github.com>
2024-03-02 23:34:49 -0600

    Cframe solo falta board (#3)
    * solo pendiente board
    
    -se le puede dar click al table widget por medio de slot
    -errores de ingreso evitados
    -información requerida necesaria
    -falta dashboard

--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -20,19 +19,29 @@ private slots:
     void on_btn_entregarM_clicked();
     void on_btn_revision_clicked();
     void on_btn_dashboardM_clicked();
+    //tab entrega
     void on_btn_sesion_clicked();
-    void on_tabWidget_tabBarClicked(int index);
     void on_tabWidget_currentChanged(int index);
-    void on_cb_facultadE_currentIndexChanged(const QString &arg1);
-
     void on_btn_silaboE_clicked();
-
     void on_btn_archivoE_clicked();
+    void on_cb_facultadE_currentIndexChanged(int index);
+    void on_btn_closeE_clicked();
+    //tab revision
+    void on_Rcb_usuario_currentIndexChanged(int index);
+    void on_Rbtn_sesion_clicked();
+    void on_Rbtn_cerrar_clicked();
+    void on_Rbtn_cambiar_clicked();
+
+
+    void on_RTW_revision_cellClicked(int row, int column);
 
 private:
     Ui::MainWindow *ui;
-    bool loginDocente=false;
-    bool loginRevision=false;
+
+    bool loginDocente=false,loginRevision=false, revision2=false;
     void limpiarEntrega();
+    void limpiarRevision();
+
+    void pruebitaBotonesTab();
 };
 #endif // MAINWINDOW_H
