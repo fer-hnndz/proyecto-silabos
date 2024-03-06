89d9b437d7fb6a5e9a5f13d487759d796e6be7b1
coleexz <williamcole@unitec.edu>
2024-03-06 15:09:35 -0600

    noseniquehice

--- Arbol.h
+++ Arbol.h
@@ -1,5 +1,6 @@
 #ifndef ARBOL_H
 #define ARBOL_H
+#include "nodoD.h"
 
 template <typename T>
 class Arbol {
@@ -7,6 +8,28 @@ class Arbol {
         Arbol<T>* left;
         Arbol<T>* right;
         T data;
+
+        // nodoD<T>* root;
+
+        // void AddAux(nodoD<T> *nodo){
+        //     if(nodo->AntPtr->AntPtr==0){
+        //         nodoD<T>* Padre = nodo->AntPtr;
+        //         Padre->AntPtr = nodo;
+        //     }else if(nodo->AntPtr->SigPtr==0){
+        //         nodoD<T>* Padre = nodo->AntPtr;
+        //         Padre->SigPtr = nodo;
+        //     }else if(nodo->SigPtr->AntPtr == 0){
+        //         nodoD<T>* Padre = nodo->SigPtr;
+        //         Padre->AntPtr = nodo;
+        //     }else if(nodo->SigPtr->SigPtr == 0){
+        //         nodoD<T>*Padre = nodo->SigPtr;
+        //         Padre->SigPtr=nodo;
+        //     }else{
+        //         AddAux(nodo->AntPtr);
+        //     }
+
+        // }
+
     public:
         Arbol<T>* getArbolIzq() {
             return left;
@@ -31,6 +54,18 @@ class Arbol {
         void setRaiz(T n) {
             data = n;
         }
+
+        // void Add(nodoD<T> nodo){
+        //     if(root==0){
+        //         root = nodo;
+        //     }else if(root->AntPtr==0){
+        //         root->AntPtr = nodo;
+        //     }else if(root->SigPtr==0){
+        //         root->SigPtr = nodo;
+        //     }else{
+        //         AddAux(nodo);
+        //     }
+        // }
 };
 
 
